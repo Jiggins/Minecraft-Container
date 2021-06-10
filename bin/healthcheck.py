44c4b21664dfffbf7e54e24d4786df9e371f7244
Jack Higgins <jackhiggins07@gmail.com>
2021-06-11 00:44:59 +0100

    Send metrics in one API call

--- bin/healthcheck.py
+++ bin/healthcheck.py
@@ -2,6 +2,7 @@
 
 import os
 from time import time
+from typing import List
 
 import boto3
 from flask import Flask
@@ -22,22 +23,34 @@ def uptime():
     return time() - start_time
 
 
-def send_metric(name: str, value, unit=None):
+def create_metric(name: str, value, unit=None, dimensions={}):
+    metric = {
+        'MetricName': name,
+        'Dimensions': [
+            {
+                'Name': 'ServerName',
+                'Value': SERVER_NAME
+            },
+        ],
+        'Value': value
+    }
+
+    for name, value in dimensions.items():
+        metric['Dimensions'].append({
+            'Name': name,
+            'Value': value
+        })
+
+    if unit:
+        metric['Unit'] = value
+
+    return metric
+
+
+def send_metrics(metrics: List[dict]):
     cloudwatch.put_metric_data(
         Namespace=NAMESPACE,
-        MetricData=[
-            {
-                'MetricName': name,
-                'Dimensions': [
-                    {
-                        'Name': 'ServerName',
-                        'Value': SERVER_NAME
-                    },
-                ],
-                'Value': value,
-                'Unit': unit
-            },
-        ]
+        MetricData=metrics
     )
 
 
@@ -46,11 +59,12 @@ def server_metrics():
         server = MinecraftServer.lookup("localhost:25565")
         status = server.status()
     except (OSError, ConnectionRefusedError) as e:
-        send_metric('Status', 0)
+        send_metrics([create_metric('Status', 0)])
 
         # With over a hundred mods Minecraft will take more than 5 minutes to
         # start up. The AWS NLB healthcheck grace period has a maximum time of
         # 5 minutes so we return a false positive until TIMEOUT is reached.
+
         if uptime() < TIMEOUT:
             return f"Server is starting up: {e}"
 
@@ -59,9 +73,11 @@ def server_metrics():
     latency = status.latency
     player_count = status.players.online
 
-    send_metric('Status', 1)
-    send_metric('Latency', latency, 'Milliseconds')
-    send_metric('PlayerCount', player_count)
+    metrics = []
+    metrics.append(create_metric('Status', 1))
+    metrics.append(create_metric('Latency', latency, 'Milliseconds'))
+    metrics.append(create_metric('PlayerCount', player_count))
+    send_metrics(metrics)
 
     return f"The server has {player_count} players and replied in {latency} ms"
 
